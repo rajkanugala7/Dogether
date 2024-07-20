50d4176db5271794a0aead2b783b0026d2b3def5
rajkanugala7 <rajkanugala7@gmail.com>
2024-07-20 12:26:25 +0530

    updated

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -126,7 +126,7 @@ const RoomPage = () => {
   return (
     <div className="room"> 
       <div className="room-info">
-      <h1>Your Meeting Room</h1>
+      <h1>Your Meeting Roommmmmm</h1>
       <h4>{remoteSocketId ? "Room has member" : "No one in room" }       {remoteSocketId && !remoteStream&& <button onClick={handleCallUser} className="btn btn-light ms-5">Call now!<i class="fa-solid fa-phone" style={{color: "green"}}></i></button>}
       </h4>
      </div>
